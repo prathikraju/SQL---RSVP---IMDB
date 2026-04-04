0e993220ed5b2de3a03355ac08096b965cfb2311
Prathik Raju <prathikraju@Mac.lan>
2026-04-04 13:07:23 -0500

    first message

--- IMDB_question.sql
+++ IMDB_question.sql
@@ -1,5 +1,7 @@
 USE imdb;
 
+-- test
+
 /* Now that you have imported the data sets, let’s explore some of the tables. 
  To begin with, it is beneficial to know the shape of the tables and whether any column has null values.
  Further in this segment, you will take a look at 'movies' and 'genre' tables.*/
